1f104197c0566d58226b0eaae39ba8b5a845b5cc
Mukwano <sparenica@outlook.com>
2025-08-27 22:51:56 +0200

    little changes

---
+++ Fxf.Blazor/Fxf.Blazor/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace Fxf.Blazor.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
---
+++ Fxf.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Fxf.Shared;
+
+public class Class1
+{
+
+}
