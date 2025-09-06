a4601ab9d2567e8027a81f11d210f1bd788290a5
Mukwano <sparenica@outlook.com>
2025-09-07 00:24:12 +0200

    LocalizationController
    Part of the Localization/Translation system - RestFul API will offer following
    
    /*
     * public GET api/localization/get_locales returns all available client locales
     * public GET api/localization/get_languages returns all supported languages with metadata
     * public GET api/localization/get_language_by_code/{code}/{isServer = false}  selected language json by code
     * public GET api/localization/get_all_dictionaries/{isServer = false} returns all available locale files as a TranslationTree
     * public POST api/localization/localize/{query}/{target}/{source} translates text using LibreTranslate default target is detected client language, source is english if not specified
     * public POST api/localization/save_locale/{code}/{isServer = false} saves a locale file for the specified language code
     */

---
+++ Fxf.Blazor/Fxf.Blazor/Api/LocalizationController.cs
@@ -0,0 +1,22 @@
+﻿using Fxf.Blazor.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace Fxf.Blazor.Api;
+[Route("api/[controller]")]
+[ApiController]
+public class LocalizationController(ILanguageService languageService, IStringLocalizer<LocalizationController> t, IHttpContextAccessor accessor) : ControllerBase
+{
+	private readonly ILanguageService _languageService = languageService;
+	private readonly IStringLocalizer<LocalizationController> _t = t;
+	private readonly IHttpContextAccessor _accessor = accessor;
+
+	/*
+	 * public GET api/localization/get_locales returns all available client locales
+	 * public GET api/localization/get_languages returns all supported languages with metadata
+	 * public GET api/localization/get_language_by_code/{code}/{isServer = false}  selected language json by code
+	 * public GET api/localization/get_all_dictionaries/{isServer = false} returns all available locale files as a TranslationTree
+	 * public POST api/localization/localize/{query}/{target}/{source} translates text using LibreTranslate default target is detected client language, source is english if not specified
+	 * public POST api/localization/save_locale/{code}/{isServer = false} saves a locale file for the specified language code
+	 */
+}
--- Fxf.Blazor/Fxf.Blazor/Middlewares/LocalizationMiddleware.cs
+++ Fxf.Blazor/Fxf.Blazor/Middlewares/LocalizationMiddleware.cs
@@ -3,10 +3,22 @@ using System.Globalization;
 
 namespace Fxf.Blazor.Middlewares;
 
+/// <summary>
+/// Middleware for setting the current request's culture based on cookies, query parameters, or Accept-Language headers.
+/// </summary>
+/// <remarks>
+/// This middleware checks for a culture value in the following order: cookie, query string, Accept-Language header. If none are found, it defaults to "en".
+/// </remarks>
 public class LocalizationMiddleware(ICookieService cookieService) : IMiddleware
 {
 	private readonly ICookieService _cookieService = cookieService;
 
+	/// <summary>
+	/// Invokes the middleware to set the current thread's culture and UI culture for the request.
+	/// </summary>
+	/// <param name="context">The current HTTP context.</param>
+	/// <param name="next">The next middleware in the pipeline.</param>
+	/// <returns>A task that represents the completion of request processing.</returns>
 	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
 	{
 		// we will check cookie first, then query and acceptLanguage hearders last - default en
@@ -23,7 +35,7 @@ public class LocalizationMiddleware(ICookieService cookieService) : IMiddleware
 			Thread.CurrentThread.CurrentUICulture = uiCulture;
 			context.Request.Headers.AcceptLanguage = cultureQuery;
 			_cookieService.SetCookie("BlazorCulture", cultureQuery, 30 * 24 * 60); // 30 days
-																										  // Check which locales are supported
+			// Check which locales are supported
 		}
 		else
 		{
@@ -34,6 +46,11 @@ public class LocalizationMiddleware(ICookieService cookieService) : IMiddleware
 		await next(context);
 	}
 
+	/// <summary>
+	/// Gets the culture value from the request by checking cookie, query string, and Accept-Language header.
+	/// </summary>
+	/// <param name="context">The current HTTP context.</param>
+	/// <returns>The culture name if found; otherwise, "en".</returns>
 	private string? GetCultureFromRequest(HttpContext context)
 	{
 		string? culture = _cookieService.GetCookie("BlazorCulture");
@@ -54,6 +71,11 @@ public class LocalizationMiddleware(ICookieService cookieService) : IMiddleware
 		return "en";
 	}
 
+	/// <summary>
+	/// Determines whether the specified culture name is valid.
+	/// </summary>
+	/// <param name="name">The culture name to validate.</param>
+	/// <returns><c>true</c> if the culture is valid; otherwise, <c>false</c>.</returns>
 	private static bool IsValidCulture(string? name)
 	{
 		return CultureInfo.GetCultures(CultureTypes.AllCultures).Any(culture => string.Equals(culture.Name, name, StringComparison.CurrentCultureIgnoreCase));
