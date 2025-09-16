89d443e72e9f04af347dfb92a4c188fd133a73b3
Mukwano <sparenica@afrowave.ltd>
2025-09-16 13:43:10 +0200

    Migration

---
+++ Fxf.Blazor/Fxf.Blazor/Data/Migrations/20250916113513_Changes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fxf.Blazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Hub",
+                table: "HubActivityLogs",
+                newName: "HubType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HubType",
+                table: "HubActivityLogs",
+                newName: "Hub");
+        }
+    }
+}
