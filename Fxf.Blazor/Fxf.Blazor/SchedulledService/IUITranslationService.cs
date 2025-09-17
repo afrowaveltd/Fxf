b66b5a2b8eccfd97ef1fdfcf192d469883c18973
Mukwano <sparenica@afrowave.ltd>
2025-09-17 09:03:28 +0200

    Comments added

--- Fxf.Blazor/Fxf.Blazor/SchedulledService/IUITranslationService.cs
+++ Fxf.Blazor/Fxf.Blazor/SchedulledService/IUITranslationService.cs
@@ -1,6 +1,13 @@
 ﻿namespace Fxf.Blazor.SchedulledService;
 
+/// <summary>
+/// Defines a service for running user interface translation operations asynchronously.
+/// </summary>
 public interface IUITranslationService
 {
+	/// <summary>
+	/// Asynchronously executes the operation represented by the implementing class.
+	/// </summary>
+	/// <returns>A task that represents the asynchronous execution of the operation.</returns>
 	Task RunAsync();
 }
