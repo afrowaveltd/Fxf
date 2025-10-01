765474f092e7bd3b0183fbeefe10842541949055
Mukwano <sparenica@afrowave.ltd>
2025-10-01 15:27:42 +0200

    Progress

--- Fxf.Mud/Data/ApplicationDbContext.cs
+++ Fxf.Mud/Data/ApplicationDbContext.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fxf.Mud.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
 {
 }
